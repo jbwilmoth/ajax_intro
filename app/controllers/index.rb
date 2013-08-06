5a3acd5b50e2db1c1539a69b77801065a95b3fbb
Kevin Solorio <soloriok@gmail.com>
2013-08-06 10:39:39 -0500

    adding new route for loading the contact form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,9 @@ get '/contacts' do
   @contacts = Contact.order('last_name ASC') # sort them by alphabetical order by last_name
   erb :index
 end
+
+# This resource is the form used to collect information about the contact
+get '/contacts/new' do
+  @contact = Contact.new(params[:contact])
+  erb :new
+end

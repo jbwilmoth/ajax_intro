9345c6ed0a5fb8010c95601f6bad582e74a734e3
Kevin Solorio <soloriok@gmail.com>
2013-08-06 10:37:54 -0500

    update route for all contacts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
-get '/:id' do
-  @contacts = Contact.order('last_name ASC') # sort them by alphabetical order
+# Shows all the contacts
+get '/contacts' do
+  @contacts = Contact.order('last_name ASC') # sort them by alphabetical order by last_name
   erb :index
 end
